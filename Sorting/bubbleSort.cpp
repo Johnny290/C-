622097ddfaa65aa1880a5b8c5e28a9bc7cc8e308
John <93053526+Johnny290@users.noreply.github.com>
2021-12-30 21:46:12 +0100

    bubbleSort

---
+++ Sorting/bubbleSort.cpp
@@ -0,0 +1,67 @@
+/*Bubble sort*/
+
+#include <iostream>
+#include <ctime>
+#include <chrono>
+using namespace std;
+
+void FillArray(int *pole, int sz)
+{
+	for (int i = 0; i < sz; i++)
+	{
+		pole[i] = -50 + rand() % 101;
+	}
+}
+
+void swap(int* p1, int* p2)
+{
+	int tmp = *p1;
+	*p1 = *p2;
+	*p2 = tmp;
+}
+
+void BubbleSort(int* pole, int sz)
+{
+	for (int i = 0; i < sz - 1; i++)
+	{
+		if (pole[i] > pole[i + 1])
+		{
+			swap(pole[i], pole[i + 1]);
+		}
+
+		for (int j = sz - 1; j > 0; j--)
+		{
+			if (pole[j] < pole[j - 1])
+			{
+				swap(pole[j], pole[j - 1]);
+			}
+		}
+	}
+}
+
+void printA(int* pole, int sz)
+{
+	for (int i = 0; i < sz; i++)
+	{
+		cout << pole[i] << " ";
+	}
+
+	cout << endl;
+}
+
+int main1()
+{
+	time_t tim = chrono::system_clock::to_time_t(chrono::system_clock::now());
+	srand((unsigned int)tim);
+
+	int sz = 10000;
+	int* pole = new int[sz];
+
+	FillArray(pole, sz);
+	printA(pole, sz);
+
+	BubbleSort(pole, sz);
+	printA(pole, sz);
+
+	return 0;
+}
